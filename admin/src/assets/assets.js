798de31d1842d0186342fdfda5072fd46f23034d
Rohit <160763948+rohitsripathi9@users.noreply.github.com>
2025-05-21 23:48:13 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -63,6 +63,7 @@ import bag_icon from './bag_icon.png'
 import logout_icon from './logout_icon.png'
 import parcel_icon from './parcel_icon.png'
 import upload_area from './upload_area.png'
+export const url ="https://mhp-hkp3.onrender.com"
 
 export const assets = {
     logo,
