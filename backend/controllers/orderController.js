1ffdc6e23b34c72b5c777bdcbec11934ba44d26c
Rohit <160763948+rohitsripathi9@users.noreply.github.com>
2025-05-22 00:18:34 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -9,7 +9,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
 });
 
 const placeOrder = async (req, res) => {
-    const frontend_url = "http://localhost:5173";
+    const frontend_url = "https://mhp-frontend.onrender.com";
     try {
         // Log the entire request for debugging
         console.log("Headers:", req.headers);
